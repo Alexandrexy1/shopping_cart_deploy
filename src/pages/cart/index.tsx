01050723d7da4d2f45658d838ef869c56dc75dd3
Alexandre Nascimento <Alexandrenas2003@gmail.com>
2023-11-22 11:06:11 -0300

    cart page

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,7 +1,29 @@
+import { CgMathPlus } from "react-icons/cg";
+import { CgMathMinus } from "react-icons/cg";
+
+
 export function Cart() {
     return(
-        <div>
-            <h1>Página de cart</h1>
+        <div className='w-full max-w-6xl mx-auto px-4 my-3'>
+            <h1 className='font-bold p-10 mx-auto text-center text-xl'>Meu carrinho</h1>
+
+            <div className='w-full mt-5'>
+                <section className='border-b-2 flex justify-between items-center'>
+                    <img src="https://d1sfzvg6s5tf2e.cloudfront.net/Custom/Content/Products/49/95/4995_fone-airpods-pro-magsafe-mqd83am-2nd-imp_m1_638072372640312793.jpg" alt="Logo do produto"
+                    className='w-28'/>
+                    <b>R$ 199,90</b>
+                    <div className='flex gap-3'>
+                        <button className='bg-slate-800 text-white rounded-md flex justify-center items-center p-1'>
+                            <CgMathMinus size={20}/>
+                        </button>
+                        <span>1</span>
+                        <button className='bg-slate-800 text-white rounded-md flex justify-center items-center p-1'>
+                            <CgMathPlus size={20}/>
+                        </button>
+                    </div>
+                    <b>Subtotal R$ 199,90</b>
+                </section>
+            </div>
         </div>
     )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,89 @@
+import { BsCartPlus } from "react-icons/bs";
+
+
 export function Home() {
     return(
-        <div>
-            <h1>Home</h1>
-        </div>
+        <main className='w-full max-w-6xl mx-auto my-3'>
+            <h1 className='font-bold p-10 text-xl text-center'>Produtos em alta</h1>
+            <div className='flex gap-12 flex-wrap'>
+                <section className='flex flex-col w-56 max-md:w-40'>
+                    <img src="https://d1sfzvg6s5tf2e.cloudfront.net/Custom/Content/Products/49/95/4995_fone-airpods-pro-magsafe-mqd83am-2nd-imp_m1_638072372640312793.jpg" alt="Logo do produto" 
+                    className='w-full'/>
+                    <p className='font-medium text-center'>Airpods apple, com Estojo de Recarga, Bluetooth</p>
+                    <div className='mt-1 flex gap-2'>
+                        <p className='font-medium text-gray-700 ml-1'>R$ 179,90</p>
+                        <button className='bg-slate-900 p-1 rounded-lg'>
+                            <BsCartPlus size={20} color='#fff'/>
+                        </button>
+                    </div>
+                </section>
+                <section className='flex flex-col w-56 max-md:w-40'>
+                    <img src="https://d1sfzvg6s5tf2e.cloudfront.net/Custom/Content/Products/49/95/4995_fone-airpods-pro-magsafe-mqd83am-2nd-imp_m1_638072372640312793.jpg" alt="" 
+                    className='w-full'/>
+                    <p className='font-medium text-center'>Airpods apple, com Estojo de Recarga, Bluetooth</p>
+                    <div className='mt-1 flex gap-2'>
+                        <p className='font-medium text-gray-700 ml-1'>R$ 179,90</p>
+                        <button className='bg-slate-900 p-1 rounded-lg'>
+                            <BsCartPlus size={20} color='#fff'/>
+                        </button>
+                    </div>
+                </section>
+                <section className='flex flex-col w-56 max-md:w-40'>
+                    <img src="https://d1sfzvg6s5tf2e.cloudfront.net/Custom/Content/Products/49/95/4995_fone-airpods-pro-magsafe-mqd83am-2nd-imp_m1_638072372640312793.jpg" alt="" 
+                    className='w-full'/>
+                    <p className='font-medium text-center'>Airpods apple, com Estojo de Recarga, Bluetooth</p>
+                    <div className='mt-1 flex gap-2'>
+                        <p className='font-medium text-gray-700 ml-1'>R$ 179,90</p>
+                        <button className='bg-slate-900 p-1 rounded-lg'>
+                            <BsCartPlus size={20} color='#fff'/>
+                        </button>
+                    </div>
+                </section>
+                <section className='flex flex-col w-56 max-md:w-40'>
+                    <img src="https://d1sfzvg6s5tf2e.cloudfront.net/Custom/Content/Products/49/95/4995_fone-airpods-pro-magsafe-mqd83am-2nd-imp_m1_638072372640312793.jpg" alt="" 
+                    className='w-full'/>
+                    <p className='font-medium text-center'>Airpods apple, com Estojo de Recarga, Bluetooth</p>
+                    <div className='mt-1 flex gap-2'>
+                    <p className='font-medium text-gray-700 ml-1'>R$ 179,90</p>
+                        <button className='bg-slate-900 p-1 rounded-lg'>
+                            <BsCartPlus size={20} color='#fff'/>
+                        </button>
+                    </div>
+                </section>
+                <section className='flex flex-col w-56 max-md:w-40'>
+                    <img src="https://d1sfzvg6s5tf2e.cloudfront.net/Custom/Content/Products/49/95/4995_fone-airpods-pro-magsafe-mqd83am-2nd-imp_m1_638072372640312793.jpg" alt="" 
+                    className='w-full'/>
+                    <p className='font-medium text-center'>Airpods apple, com Estojo de Recarga, Bluetooth</p>
+                    <div className='mt-1 flex gap-2'>
+                        <p className='font-medium text-gray-700 ml-1'>R$ 179,90</p>
+                        <button className='bg-slate-900 p-1 rounded-lg'>
+                            <BsCartPlus size={20} color='#fff'/>
+                        </button>
+                    </div>
+                </section>
+                <section className='flex flex-col w-56 max-md:w-40'>
+                    <img src="https://d1sfzvg6s5tf2e.cloudfront.net/Custom/Content/Products/49/95/4995_fone-airpods-pro-magsafe-mqd83am-2nd-imp_m1_638072372640312793.jpg" alt="" 
+                    className='w-full'/>
+                    <p className='font-medium text-center'>Airpods apple, com Estojo de Recarga, Bluetooth</p>
+                    <div className='mt-1 flex gap-2 ml-1'>
+                        <p className='font-medium text-gray-700 ml-1'>R$ 179,90</p>
+                        <button className='bg-slate-900 p-1 rounded-lg'>
+                            <BsCartPlus size={20} color='#fff'/>
+                        </button>
+                    </div>
+                </section>
+                <section className='flex flex-col w-56 max-md:w-40'>
+                    <img src="https://d1sfzvg6s5tf2e.cloudfront.net/Custom/Content/Products/49/95/4995_fone-airpods-pro-magsafe-mqd83am-2nd-imp_m1_638072372640312793.jpg" alt="" 
+                    className='w-full'/>
+                    <p className='font-medium text-center'>Airpods apple, com Estojo de Recarga, Bluetooth</p>
+                    <div className='mt-1 flex gap-2'>
+                        <p className='font-medium text-gray-700 ml-1'>R$ 179,90</p>
+                        <button className='bg-slate-900 p-1 rounded-lg'>
+                            <BsCartPlus size={20} color='#fff'/>
+                        </button>
+                    </div>
+                </section>
+            </div>
+        </main>
     )
 }
